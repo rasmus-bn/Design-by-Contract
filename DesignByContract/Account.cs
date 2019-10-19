cd34822476c28c6caf287a6adc5765decb8e353a
OnkelDunkel <rasbalnor@gmail.com>
2019-10-19 16:43:20 +0200

    initiated project

---
+++ DesignByContract/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace DesignByContract
+{
+    public class Account
+    {
+        public void Deposit(double amount)
+        {
+
+        }
+
+        public void Withdraw(double amount)
+        {
+
+        }
+    }
+}
